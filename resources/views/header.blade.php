30afcc2abdc56bab96bba58173cecf778f8671fa
RoccoCerro <rcerro02@gmail.com>
2024-05-20 15:22:17 +0200

    Add: list style

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,12 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Header</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-  <ul>
-    <li><a href="http://127.0.0.1:8000/">Home</a></li>
-    <li><a href="http://127.0.0.1:8000/registration">Registration</a></li>
-    <li><a href="http://127.0.0.1:8000/login">Login</a></li>
+  <ul class="list-group list-group-horizontal p-3">
+    <li class="list-group-item"><a href="http://127.0.0.1:8000/">Home</a></li>
+    <li class="list-group-item"><a href="http://127.0.0.1:8000/registration">Registration</a></li>
+    <li class="list-group-item"><a href="http://127.0.0.1:8000/login">Login</a></li>
   </ul>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
