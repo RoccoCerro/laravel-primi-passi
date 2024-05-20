3bb512f3d7ff640994211f0d2e74f3bd141515af
RoccoCerro <rcerro02@gmail.com>
2024-05-20 16:01:46 +0200

    Refacotor: home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,28 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Primi passi laravel</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-  <?php
-  require_once(__DIR__ ."/../../../resources/views/header.blade.php");
-  var_dump(__DIR__);
-  ?>
-  <h1>Hello World</h1>
+  <header>
+    <?php
+    require_once(__DIR__ ."/../../../resources/views/header.blade.php");
+    var_dump(__DIR__);
+    ?>
+  </header>
+
+  <main>
+    <h1>Hello World</h1>
+
+  </main>
+
+  <footer>
+    <?php
+    require_once(__DIR__ ."/../../../resources/views/footer.blade.php");
+    var_dump(__DIR__);
+    ?>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
