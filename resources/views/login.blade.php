b6ea497b97abd2019c761d96fa3592e979c41922
RoccoCerro <rcerro02@gmail.com>
2024-05-20 15:46:40 +0200

    Add: style login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,13 +4,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-  <form action="">
-    <label for="surname">Surname</label>
-    <input type="text" name="" id="surname">
-    <label for="psw">Password</label>
-    <input type="password" name="" id="psw">
-  </form>
+  <div class="container">
+    <form action="" class="p-2">
+      <div class="mb-3">
+        <label class="form-label" for="surname">Surname</label>
+        <input class="form-control" type="text" name="" id="surname">
+      </div>
+      <div class="mb-3">
+        <label class="form-label" for="psw">Password</label>
+        <input class="form-control" type="password" name="" id="psw">
+      </div>
+      <button >Entra</button>
+    </form>
+    <form action="http://127.0.0.1:8000/" class="p-2">
+      <button >Home</button>
+    </form>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
