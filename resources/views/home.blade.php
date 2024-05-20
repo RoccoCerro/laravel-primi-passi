adbf42e5909da36a2971aad5a44f45d6b4987bca
RoccoCerro <rcerro02@gmail.com>
2024-05-20 15:14:43 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Primi passi laravel</title>
+</head>
+<body>
+  <?php
+  require_once(__DIR__ ."/../../../resources/views/header.blade.php");
+  var_dump(__DIR__);
+  ?>
+  <h1>Hello World</h1>
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="">
+    <label for="surname">Surname</label>
+    <input type="text" name="" id="surname">
+    <label for="psw">Password</label>
+    <input type="password" name="" id="psw">
+  </form>
+</body>
+</html>
